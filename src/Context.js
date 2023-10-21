34bd90785b61502b64e3301e49a59afdb990124e
Beyond <addykyaw3@gmail.com>
2023-10-21 19:25:07 +0630

    fourth commit

--- src/Context.js
+++ src/Context.js
@@ -4,9 +4,11 @@ import { Navigate, useLocation, useNavigate } from "react-router-dom";
 
 const AnimeContext = React.createContext();
 
+//https://blue-violet-bee-vest.cyclic.app/
+//https://anime-myanmar-server.vercel.app/
 // "https://anime-myanmar-database-23.herokuapp.com/"
 function Context(props) {
-  const url = "https://anime-myanmar-server.vercel.app/";
+  const url = "http://localhost:5000/";
   const url2 = "https://anime-myanmar.vercel.app";
   const [anime, setAnime] = React.useState([]);
   const [Mload, setMload] = React.useState(false);
